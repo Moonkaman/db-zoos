093dc8405c9de473327bbab905ec1d6545e3c688
Moonkaman <nickbatmanglidj@gmail.com>
2019-02-19 14:24:46 -0700

    Create put endpoint for /api/zoos/:id

--- index.js
+++ index.js
@@ -43,6 +43,22 @@ server.post('/api/zoos', (req, res) => {
   }
 })
 
+server.put('/api/zoos/:id', (req, res) => {
+  if(req.body.name){
+    db('zoos').where({id: req.params.id}).update(req.body)
+      .then(count => {
+        if(count > 0) {
+          db('zoos').where({id: req.params.id}).first()
+            .then(updatedZoo => res.status(200).json(updatedZoo))
+            .catch(err => res.status(500).json({errorMessage: 'Could not retrieve the updated zoo at this time', error: err}));
+        }
+      })
+      .catch(err => res.status(500).json({errorMessage: 'Could not update the specified zoo at this time', error: err}));
+  } else {
+    res.status(400).json({errorMessage: 'Please provide a name'})
+  }
+})
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
